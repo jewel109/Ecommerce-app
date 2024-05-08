b1cce7b2f6e15ec03abfb69a89375dd8a2f4d91d
jewel109 <raihanurrahmanjewel@gmail.com>
2024-05-08 21:53:11 +0600

    customer schema

---
+++ src/db/schema/customerSchema.ts
@@ -0,0 +1,15 @@
+import { boolean, pgSchema, serial, text, varchar, timestamp } from "drizzle-orm/pg-core";
+
+
+export const ecommerce_schema = pgSchema("ecommerce_schema")
+
+
+export const customers_schema = ecommerce_schema.table("customer", {
+  id: serial("id").primaryKey(),
+  name: text("name").unique(),
+  email: text("email").unique(),
+  password: text("password"),
+  active: boolean("active"),
+  registered_at: timestamp("registered_at").defaultNow()
+})
+
